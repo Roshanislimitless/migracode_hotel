36c0314f65c0ca8dc7e6bd6cb28cf4f9ebaacc71
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-05-13 03:57:44 +0200

    updated my hotel with slideshow photos

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+let galleryIndex = 1;
+let numberOfImages = 7;
+
+function allFunctions(){
+     var forwardButton =   document.querySelector('#gallery-forward');
+    forwardButton.addEventListener("click", forward);
+
+    var backwardButton = document.querySelector('#gallery-backward');
+    backwardButton.addEventListener('click', backward);
+
+    preventDefaut();    
+
+}
+
+allFunctions();
+
+function forward(){
+    if (galleryIndex  <= numberOfImages) {
+        galleryIndex++
+    }
+    if (galleryIndex > numberOfImages ) {
+        galleryIndex = 1;
+    }
+    getImageUrl();
+    preventDefaut();
+}
+
+function backward(){
+    if ( galleryIndex >= 1){
+        galleryIndex--
+    }
+    if (galleryIndex < 1 ){
+        galleryIndex = numberOfImages;
+    }
+    getImageUrl();
+    preventDefaut();
+
+}
+
+function getImageUrl() {
+    let imageSrc = document.querySelector('#image-collection');
+    imageSrc.src = "/photos/room" + galleryIndex + ".jpg"
+}
+
